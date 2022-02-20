189b3c2afca50b86c8d12da06e3fa10dfba4e930
chefarbeiter <github@jann.bayern>
2022-02-20 19:01:50 +0100

    Add unit tests

--- tests/buttons.spec.js
+++ tests/buttons.spec.js
@@ -22,4 +22,93 @@ describe('A button in the conformation dialog', function () {
         openPopups = document.querySelectorAll(".are-you-sure");
         expect(openPopups.length).toBe(0);
     });
+
+    it("should close the confirmation if it is a confirm button", function () {
+        let fakeButton = document.createElement("button");
+        let confirmation = new Confirmation(fakeButton);
+        fakeButton.click();
+
+        let openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(1);
+
+        let yesButton = document.querySelector(".ays-yes");
+        yesButton.click();
+
+        openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(0);
+    });
+
+    it("should call the onConfirm callback if it is a confirm button", function () {
+        let fakeButton = document.createElement("button");
+        let confirmCallback = jasmine.createSpy("confirm");
+        let dismissCallback = jasmine.createSpy("dismiss");
+        let config = { onConfirm: confirmCallback, onDismiss: dismissCallback };
+        let confirmation = new Confirmation(fakeButton, config);
+        fakeButton.click();
+
+        let openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(1);
+
+        let yesButton = document.querySelector(".ays-yes");
+        yesButton.click();
+
+        expect(confirmCallback).toHaveBeenCalledTimes(1);
+        expect(dismissCallback).not.toHaveBeenCalled();
+    });
+
+    it("should call the onDismiss callback if it is a cancel button", function () {
+        let fakeButton = document.createElement("button");
+        let confirmCallback = jasmine.createSpy("confirm");
+        let dismissCallback = jasmine.createSpy("dismiss");
+        let config = { onConfirm: confirmCallback, onDismiss: dismissCallback };
+        let confirmation = new Confirmation(fakeButton, config);
+        fakeButton.click();
+
+        let openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(1);
+
+        let noButton = document.querySelector(".ays-no");
+        noButton.click();
+
+        expect(dismissCallback).toHaveBeenCalledTimes(1);
+        expect(confirmCallback).not.toHaveBeenCalled();
+    });
+
+
+    it("should call the onDismiss callback if it is a cancel button and not hide if the callback returns something", function () {
+        let fakeButton = document.createElement("button");
+        let config = { onDismiss: () => "something" };
+        let confirmation = new Confirmation(fakeButton, config);
+        fakeButton.click();
+
+        let openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(1);
+
+        let noButton = document.querySelector(".ays-no");
+        noButton.click();
+
+        openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(1);
+    });
+
+    it("should call a custom callback", function () {
+        let fakeButton = document.createElement("button");
+        let customCallback = jasmine.createSpy("callback");
+        let config = {
+            buttons: [{
+                label: 'My Custom Button',
+                onClick: customCallback,
+                classes: 'my-button'
+            }]
+        };
+        let confirmation = new Confirmation(fakeButton, config);
+        fakeButton.click();
+
+        let openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(1);
+
+        let myButton = document.querySelector(".my-button");
+        myButton.click();
+        expect(customCallback).toHaveBeenCalledTimes(1);
+    });
 });
--- tests/trigger.spec.js
+++ tests/trigger.spec.js
@@ -1,6 +1,13 @@
 import { Confirmation } from '../src/confirmation'
 
 describe('The trigger button', function () {
+    beforeEach(function () {
+        let openDialogs = document.querySelectorAll(".are-you-sure");
+        for (const dialog of openDialogs) {
+            dialog.remove()
+        }
+    });
+
     it("should show the confirmation on click", function () {
         let fakeButton = document.createElement("button");
         let confirmation = new Confirmation(fakeButton);
@@ -9,5 +16,31 @@ describe('The trigger button', function () {
 
         let openPopups = document.querySelectorAll(".are-you-sure");
         expect(openPopups.length).toBe(1);
+    });
+
+    it("should not show the confirmation if already visible", function () {
+        let fakeButton = document.createElement("button");
+        let confirmation = new Confirmation(fakeButton);
+
+        fakeButton.click();
+
+        let openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(1);
+
+        fakeButton.click();
+
+        openPopups = document.querySelectorAll(".are-you-sure");
+        expect(openPopups.length).toBe(1);
+    });
+
+    it("should call the onClick callback on click", function () {
+        let callbackSpy = jasmine.createSpy("onClick_Callback");
+        let config = { onClick: callbackSpy };
+        let fakeButton = document.createElement("button");
+        let confirmation = new Confirmation(fakeButton, config);
+
+        fakeButton.click();
+
+        expect(callbackSpy).toHaveBeenCalledTimes(1);
     });
 });
